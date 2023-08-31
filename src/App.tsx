b91a828a2d06f1fd5d9bf718787a46329be8eb4d
sirwhod <rodrigo.brandao98@gmail.com>
2023-08-30 23:16:03 -0300

    Criacao da base do projeto e configuracoes iniciais

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GithubLogo } from './assets/icons/GithubLogo'
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-base-background">
+      <GithubLogo />
+      <h1 className="text-2xl font-semibold text-base-text">
+        Hello Vite + tailwindcss
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: 'Nunito, sans serif',
+      },
+      lineHeight: {
+        base: '160%',
+      },
+      colors: {
+        blue: '#3294F8',
+        'base-title': '#E7EDF4',
+        'base-subtitle': '#C4D4E3',
+        'base-text': '#AFC2D4',
+        'base-span': '#7B96B2',
+        'base-label': '#7B96B2',
+        'base-border': '#1C2F41',
+        'base-post': '#112131',
+        'base-profile': '#0B1B2B',
+        'base-background': '#071422',
+        'base-input': '#040F1A',
+      },
+    },
+  },
+  plugins: [],
+}
